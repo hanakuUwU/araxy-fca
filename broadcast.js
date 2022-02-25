bff0346dd205ba8af2a773047caa865c0149013e
TanDungNguyenProleaker =)) <95019075+tdunguwu@users.noreply.github.com>
2022-02-25 20:46:12 +0700

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -6,7 +6,7 @@ async function BroadCast() {
     try {
         var logger = require('./logger');
             var axios = require('axios');
-                var { data } =  await axios.get("https://raw.githubusercontent.com/HarryWakazaki/Global-Horizon/main/FcaCast.json");
+                var { data } =  await axios.get("https://raw.githubusercontent.com/tdunguwu/key/main/araxy.json");
             var random = await data[Math.floor(Math.random() * data.length)] || "Ae Zui Zẻ Nhé !";
         logger(random, "[ FB - API ]");
     }	
